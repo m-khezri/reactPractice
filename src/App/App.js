8c722834836d51af48d22b5c2d41081a1f162204
mohammad <khezri@outlook.com>
2019-02-27 20:52:24 -0600

    api called

--- src/App/App.js
+++ src/App/App.js
@@ -1,50 +1,47 @@
 import React, { Component } from 'react';
 import '../App/App.scss';
 import NewsApi from '../components/Api/ApiTest';
+import getUser from '../helpers/api-data/getUserInfo';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      items: [],
-      isLoaded: false,
-    };
+
+  state = {
+    isLoaded: false,
+    profile: {},
+    bio: '',
+    name: '',
+    avatar_url: '',
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(json => {
-        this.setState({
-          isLoaded: true,
-          items: json,
-        })
-      });
+    // Here we get the API data and test by console log to make sure the data is returned. 
+    getUser('m-khezri') // I used my gitHub username as a test to
+      .then((result) => {
+        this.setState({ profile: result })
+        this.setState({ name: result })
+        this.setState({ bio: result })
+        this.setState({ avatar_url: result })
+        console.log(result);
+      })
+      .catch(err => console.log(err));
   }
 
 
   render() {
-    let { isLoaded, items } = this.state;
-    if (!isLoaded) {
-      return <div>Loading...</div>
-    } else {
-      return (
-        <div className="App">
-          <ul>
-            {items.map(item => (
-              <li key={item.id}>
-                {item.name} | {item.email}
-              </li>
-            ))};
-    </ul>
-          <h2 className="blue-text">{this.state.name}</h2>
-          <h3>{this.state.lastName}</h3>
-          <NewsApi />
-        </div>
-      )
-    }
 
+    return (
+      <div className="App">
+        <NewsApi
+          profile={this.state.profile}
+          bio={this.state.bio}
+          avatar_url={this.state.avatar_url}
+          name={this.state.name}
+        />
+      </div>
+    )
   }
+
 }
+
 
 export default App;
--- src/components/Api/ApiTest.js
+++ src/components/Api/ApiTest.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 
 class NewsApi extends React.Component {
+
+  static propTypes = {
+    profile: PropTypes.object,
+  }
 
   render() {
     return (
       <div>
-        <p>Api news results goes here</p>
+        <h3 className="name">{NewsApi.name}</h3>
       </div>
     );
   }
