0a69d2abb7af99306dd57f8362f7611314b1b26f
mohammad <khezri@outlook.com>
2019-03-08 16:12:41 -0600

    testing api

--- src/components/Api/ApiTest.js
+++ src/components/Api/ApiTest.js
@@ -11,7 +11,7 @@ class NewsApi extends React.Component {
   render() {
     return (
       <div>
-        <h3 className="name">{NewsApi.name}</h3>
+        <h3{profile.name}</h3>
       </div>
     );
   }
