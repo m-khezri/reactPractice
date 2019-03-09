d8d329cad1d27a8723b70fb32d3d41809765feb1
mohammad <khezri@outlook.com>
2019-03-09 11:13:49 -0600

    print

--- src/components/Api/ApiTest.js
+++ src/components/Api/ApiTest.js
@@ -11,7 +11,6 @@ class NewsApi extends React.Component {
   render() {
     return (
       <div>
-        <h3{profile.name}</h3>
       </div>
     );
   }
---
+++ src/components/print/print.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class print extends React.Component {
+  render() {
+    return (
+
+      <h1 className="display-1 text-light">Print</h1>
+
+    );
+  }
+}
+
+export default print;
