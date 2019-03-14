79f5a02888f8c9f016b801b54115f4510b6c4a0e
mohammad <khezri@outlook.com>
2019-03-13 19:44:17 -0500

    printed the result

--- src/components/print/print.js
+++ src/components/print/print.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-class print extends React.Component {
+class Print extends React.Component {
   render() {
     return (
-      <h1 className="display-1 text-light">Print</h1>
+      <div>
+        <h1 className="display-1 text-light">Results goes here</h1>
+        <h2>{this.props.profile.bio}</h2>
+        <h6>{this.props.profile.name}</h6>
+        <img src={this.props.profile.avatar_url} alt="users pic" />
+        {/* <h2>{this.props.profile}</h2> */}
+      </div>
     );
   }
 }
 
-export default print;
+export default Print;
