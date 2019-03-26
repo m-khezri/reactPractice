188255889e72532df5bb80cf3e89819f5d8f4a62
mohammad <khezri@outlook.com>
2019-03-26 12:04:41 -0500

    routing

--- src/components/print/print.js
+++ src/components/print/print.js
@@ -3,11 +3,10 @@ import React from 'react';
 class Print extends React.Component {
   render() {
     return (
-      <div>
-        <h1 className="display-1 text-light">Results goes here</h1>
-        <h2>{this.props.profile.bio}</h2>
-        <h6>{this.props.profile.name}</h6>
-        <img src={this.props.profile.avatar_url} alt="users pic" />
+      <div className="bg-light m-5 p-5 rounded">
+        <img className="rounded shadow" src={this.props.profile.avatar_url} alt="users pic" />
+        <h2 className="mt-3">{this.props.profile.name}</h2>
+        <h6>{this.props.profile.bio}</h6>
         {/* <h2>{this.props.profile}</h2> */}
       </div>
     );
