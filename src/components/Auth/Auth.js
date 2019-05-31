5e6a1ed6bbe70d29357e2ac93bfd400075d9dd38
mohammad <khezri@outlook.com>
2019-05-30 22:53:04 -0500

    added API keys and Auth component

---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Auth.scss';
+
+class Auth extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>Login</button>
+      </div>
+    );
+  }
+}
+
+export default Auth;
