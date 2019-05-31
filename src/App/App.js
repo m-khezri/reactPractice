05354c75749c9d65a2d6a12ab293a45bc0961a79
mohammad <khezri@outlook.com>
2019-05-30 23:07:47 -0500

    added firebase connection

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import '../App/App.scss';
+import connection from '../helpers/connection';
+import Auth from '../components/Auth/Auth';
+
+
+
 
 class App extends Component {
+
+  componentDidMount() {
+    connection();
+  }
+
   render() {
     return (
       <div className="App">
         <h2 className="blue-text">I'm learning react + Materilize</h2>
-
+        <Auth />
       </div>
     );
   }
