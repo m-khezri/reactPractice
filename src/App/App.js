ea3732b95743f1cc4b0de9ac9bb558c2373a6835
mohammad <khezri@outlook.com>
2019-08-13 23:16:45 -0500

    error getting data

--- src/App/App.js
+++ src/App/App.js
@@ -1,48 +1,33 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import '../App/App.scss';
-import getUser from '../helpers/api-data/getUserInfo';
-import NavBar from '../components/NavBar/NavBar';
-import Print from '../components/print/print';
-import About from '../components/About/About';
-import UserInput from '../components/Input/UserInput';
-import UserOutput from '../components/Output/UserOutput';
+import React from 'react';
+import './App.scss';
+import CarsInput from '../components/CarsInput/CarsInput';
+import getMakes from '../helpers/MakeGetter';
 
-class App extends Component {
-
+class App extends React.Component {
   state = {
-    profile: {},
-    username: 'Zahra',
-  }
-
-  userNameChangeHandler = (e) => {
-    this.setState({ username: e.target.value })
+    makes: []
   }
 
   componentDidMount() {
-    // Here we get the API data and test by console log to make sure the data is returned. 
-    getUser('m-khezri') // I used my gitHub username as a test to
+
+
+    getMakes.getMakes('makes')
       .then((result) => {
         console.log(result);
-        this.setState({ profile: result });
+        this.setState({ makes: result });
       })
       .catch(err => console.log(err));
   }
 
   render() {
     return (
-      <BrowserRouter>
-        <NavBar />
-        <Switch>
-          <Print profile={this.state.profile} />
-          <Route exact path="/" Component={Print} />
-          <Route path="/about" Component={About} />
-        </Switch>
-        <UserInput changed={this.userNameChangeHandler} currentName={this.state.username} />
-        <UserOutput userName={this.state.username} />
-      </BrowserRouter>
+      <div>
+        <h1>cars</h1>
+        <CarsInput />
+      </div>
     );
   }
 }
+
 
 export default App;
